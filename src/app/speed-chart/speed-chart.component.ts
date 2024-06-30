304e0d2fcb47778396f6d4da1e81bdcc8cb4e365
Edward Kirakosyan <kirakosyan3010@gmail.com>
2024-06-30 09:02:12 +0400

    feat: added speed-chart component and changed some configurations

---
+++ src/app/speed-chart/speed-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-speed-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './speed-chart.component.html',
+  styleUrl: './speed-chart.component.scss'
+})
+export class SpeedChartComponent {
+
+}
